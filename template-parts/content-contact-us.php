7df625ac5ec51255fce49382c42c3eae6841ee79
chengqi <chengqi@chengqideMacBook-Pro.local>
2021-04-12 11:47:00 -0700

    style contact page

--- template-parts/content-contact-us.php
+++ template-parts/content-contact-us.php
@@ -1,4 +1,12 @@
 <?php ?>
-<article <?php post_class(); ?>>
-  <?php the_content(); ?>
+<article class="contact-page">
+
+<h2 class="contact-page-title"><?php the_title(); ?></h2>
+
+<div class="contact-page-content-wrapper">
+<div class="contact-page-img"><?php  
+  the_post_thumbnail(); ?></div>
+
+<div class="contact-page-form"><?php  
+  the_content(); ?></div></div>
 </article>
