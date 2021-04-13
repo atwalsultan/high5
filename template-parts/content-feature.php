2b35b5bea508a9d7bc69dbdec34623e4b8f76a2a
Sultan <sultan.singh.atwal@gmail.com>
2021-04-13 10:01:18 -0700

    Integrate and improve styles for first 4 pages

--- template-parts/content-feature.php
+++ template-parts/content-feature.php
@@ -1,9 +1,10 @@
 <?php ?>
   <article <?php post_class(); ?>>
   <div class="article-content">
+    <img src="<?php the_field('hand_icon')  ?>" class="hand-icon" alt="Hand Icon">
     <h3 class="feature-title"><?php the_title(); ?></h3>
     <div class="feature-desc"><?php the_field('feature_description'); ?></div>
-    <a class="ext-link" href="high5.wmdd.ca"><?php the_field('external_link'); ?></a>
+    <a class="ext-link" href="https://high5.wmdd.ca/index.html" target="_blank"><?php the_field('link_text'); ?></a>
   </div>
   <div class="article-image">
     <?php  the_post_thumbnail(); ?>
